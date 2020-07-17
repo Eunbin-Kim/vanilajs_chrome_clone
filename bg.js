683d10c9aae14ed2c7e755824afd2f5e0e595c87
Eunbin-Kim <qpmz1212@naver.com>
2020-07-17 14:40:46 +0900

    Finished making clone App
    Finished making clone App

--- bg.js
+++ bg.js
@@ -4,8 +4,9 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
     const image = new Image();
-    img.src = `/images/${imgNumber + 1}.jpg`;
-    bod
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
 }
 
 function genRandom(){
